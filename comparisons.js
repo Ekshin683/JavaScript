8c2be81c80c30e09e98cdaaaddcd47f50ebaf3b9
Ekshin683 <ekshindeep@gmail.com>
2026-02-10 13:48:29 +0530

    update

---
+++ comparisons.js
@@ -0,0 +1,14 @@
+let a = 10
+let b = 20
+console.log(a > b) //false
+console.log(a < b) //true
+console.log(a >= b) //false
+console.log(a <= b) //true
+console.log(a == b) //false
+console.log(a != b) //true
+
+
+let x = "10"
+let y = 10
+console.log(x == y) //true (loose equality)
+console.log(x === y) //false (strict equality)
