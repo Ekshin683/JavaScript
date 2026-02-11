152290aa4f888ead76e4da83a17bd76fb3b03b3c
Ekshin683 <ekshindeep@gmail.com>
2026-02-11 16:10:25 +0530

    update

---
+++ file11.js
@@ -0,0 +1,14 @@
+// const add = (a,b) => {
+//     return a+b
+// }
+// const result = add(10,20)
+// console.log(result)
+
+// const add = (a,b) => a+b
+// const result = add(10,20)
+// console.log(result)
+
+const sqr = a => a**a
+
+const result = sqr(5)
+console.log(result)
