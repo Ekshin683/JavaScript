7d8b871ee487cfb30664b5fa60e5e23b06c2ef32
Ekshin683 <ekshindeep@gmail.com>
2026-02-11 14:02:34 +0530

    update

--- forloop.js
+++ forloop.js
@@ -16,6 +16,10 @@
 //     console.log(i)
 //}
 
-for(let i=5;i<=30;i+=5){
-    console.log(i)
+// for(let i=5;i<=30;i+=5){
+//     console.log(i)
+// }
+
+for(let i=1;i<=10;i++){
+    console.log("2 * " + i + " = " + 2 * i)
 }
