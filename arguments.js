1baa4f19b6afe2b041d701b5eec45adefc6d905a
Ekshin683 <ekshindeep@gmail.com>
2026-02-13 16:54:53 +0530

    update

--- arguments.js
+++ arguments.js
@@ -12,8 +12,17 @@
 // }
 // add(10,20,30,40)
 
-function add(...arr){
-    return arr.reduce((sum, a) => sum+a,0)
+// function add(...arr){
+//     return arr.reduce((sum, a) => sum+a,0)
+// }
+// const result = add(7,5,8,9)
+// console.log(result)
+
+
+const add = (...args) => {
+    let sum = args.reduce((sum,a) => sum+a,0)
+    return sum;
 }
-const result = add(7,5,8,9)
-console.log(result)
+
+const results = add(7,5,8,9);
+console.log(results)
