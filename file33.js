1a13eb09ce6894cd9d176fc37a9fb742f39e9e57
Ekshin683 <ekshindeep@gmail.com>
2026-02-16 10:41:35 +0530

    update

--- file33.js
+++ file33.js
@@ -1,7 +1,8 @@
-const student = {
-    name:"Ekshin",
-    age:21
-}
+// const student = {
+//     name:"Ekshin",
+//     age:21,
+//     // city: "Jalandhar"
+// }
 //destructuring - assigning object properties to variables
 // names = ["Ekshin","Ajay","Sumanth"]
 // const [name1,name2,name3] = names;
@@ -9,6 +10,27 @@ const student = {
 // console.log(name2);
 // console.log(name3);
 
-const {name,age}= student;
-console.log(name);
-console.log(age);
+// const {name,age}= student;
+// console.log(name);
+// console.log(age);
+// console.log(student.name);
+
+// const {name: userName} = student;
+// console.log(userName);
+//default value
+// const {city="Jalandhar"} = student;
+// console.log(city);
+// console.log(student);
+
+const student = {
+    name:"Ekshin",
+    age: 21,
+    marks:{
+        math: 85,
+        science: 90
+    }
+}
+const {marks} = student
+console.log(marks);
+const {marks:{math}} = student;
+console.log(math);
