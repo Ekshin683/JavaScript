21502d159c9299691806f8ab91949e269ca8a7ad
Ekshin683 <ekshindeep@gmail.com>
2026-02-16 10:58:27 +0530

    update

---
+++ file35.js
@@ -0,0 +1,9 @@
+// const names = ["Ekshin","Ajay","Sumanth","Satyam"];
+// const names = new Array("Ekshin","Ajay","Sumanth","Satyam");
+const names = new Array("Ekshin","Ajay","Sumanth","Satyam")
+// names.push("Suresh");
+// names.pop(); //removes the last element
+// names.shift(); //removes the first element
+// names.unshift("Ekshin"); //adds an element at the beginning
+
+console.log(names);
