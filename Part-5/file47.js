ca8c6fddbaf003b49cc04c40f2d812dc6cef2600
Ekshin683 <ekshindeep@gmail.com>
2026-02-17 11:26:49 +0530

    update

---
+++ Part-5/file47.js
@@ -0,0 +1,13 @@
+const students  = [
+    {id:1,name:"Ekshindeep",s1: true,s2:true,s3:false},
+    {id:2,name:"Ajay",s1:true,s2:true,s3:true},
+    {id:3,name:"Suresh",s1:false,s2:true,s3:true}
+]
+
+function getInfo(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            const student
+        })
+    })
+}
