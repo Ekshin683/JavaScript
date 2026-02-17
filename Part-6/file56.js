2ba6eff116b003fddc28826d323aca40a75de84a
Ekshin683 <ekshindeep@gmail.com>
2026-02-17 14:42:31 +0530

    update

--- Part-6/file56.js
+++ Part-6/file56.js
@@ -1,5 +1,14 @@
 const employees = ["Ekshin","Ajay","Mike"]
 
-for(let e in employees){
+for(let e in employees){ //index of employees
     console.log(e);
+}
+
+const student = {
+    name: "Ekshin",
+    age: 21
+}
+
+for(let s of Object.keys(student)){
+    
 }
---
+++ Part-6/file57.js
@@ -0,0 +1,5 @@
+const employees = ["Ekshin","Ajay","Mike"]
+
+for(let e of employees){ //value of employees
+    console.log(e);
+}
