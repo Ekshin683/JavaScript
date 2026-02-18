207ee241221567aa13ba948a0f07f15a293dc5b8
Ekshin683 <ekshindeep@gmail.com>
2026-02-18 13:01:21 +0530

    update

---
+++ Part-6/file58.js
@@ -0,0 +1,25 @@
+// x = 10
+// console.log(x)
+
+// "use strict"
+// x = 10
+// console.log(x);
+
+// "use strict"
+// let x = 10
+// console.log(x);
+
+// "use strict"
+// function add(a,a){
+//     console.log(a+a)
+// }
+// add(10,20)
+
+// "use strict"
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10,20)
+// "use strict"
+// let x = 010
+// console.log(x)
